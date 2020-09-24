04ded69993fa0ec37b0a6ea37963eb6bea63b3cf
HATACKAH <godads@yandex.ru>
2020-09-24 13:27:47 +0300

    try to add form to add new room

--- client/src/components/TopicsWindow/TopicsWindow.js
+++ client/src/components/TopicsWindow/TopicsWindow.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React , { useState } from 'react'
 import './TopicsWindow.css';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { Link } from "react-router-dom";
 
 
-export default function TopicsWindow () {
+
+export default function TopicsWindow ({name}) {
+    const [room, setRoom] = useState('');
     return (
        <List className='topic-wrapper'>
+           <div>
+               <h3> Add new room: </h3>
+               <TextField id="outlined-basic" label="Enter room name" variant="outlined" onChange={(event) => setRoom(event.target.value)}/>
+               <Link onClick={e => (!room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+               <Button variant="contained" >Add</Button>
+               </Link>
+           </div>
            {
                ['topic'].map(topic => (
                 <ListItem key={topic} button>
