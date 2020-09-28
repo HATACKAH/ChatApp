14373c3e21867e1dfa1297adbab53b6db1b454f8
HATACKAH <godads@yandex.ru>
2020-09-28 18:46:13 +0300

    del unnecessary comments

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,126 +1,135 @@
 import React, { useState, useEffect } from "react";
-import queryString from 'query-string';
-import { connect, useSelector } from 'react-redux';
+import queryString from "query-string";
+import { connect, useSelector } from "react-redux";
 import io from "socket.io-client";
 
-import Messages from '../Messages/Messages';
-import InfoBar from '../InfoBar/InfoBar';
-import Input from '../Input/Input';
-import TopicsWindow from '../TopicsWindow/TopicsWindow'
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import TopicsWindow from "../TopicsWindow/TopicsWindow";
 
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
 import {
-    setRoom,
-    setName,
-    addMessage,
-    roomUsersUpdated,
-} from '../../store/actions';
+  setRoom,
+  setName,
+  addMessage,
+  roomUsersUpdated
+} from "../../store/actions";
 
-import './Chat.css';
+import "./Chat.css";
 
-const ENDPOINT = 'localhost:5000';
+const ENDPOINT = "localhost:5000";
 
 let socket;
 const joinedRooms = [];
 
 const ChatComponent = ({
-    location,
-    setRoom,
-    setName,
-    addMessage,
-    roomUsersUpdated,
+  location,
+  setRoom,
+  setName,
+  addMessage,
+  roomUsersUpdated
 }) => {
-    const userName = useSelector(s => s.userName);
-    const rooms = useSelector(s => Object.keys(s.rooms));
+  const userName = useSelector(s => s.userName);
+  const rooms = useSelector(s => Object.keys(s.rooms));
 
+  const currentRoom = useSelector(s => s.currentRoom);
+  const users = useSelector(
+    s =>
+      (s.currentRoom &&
+        s.rooms[s.currentRoom] &&
+        s.rooms[s.currentRoom].users) ||
+      []
+  );
+  const messages = useSelector(
+    s =>
+      (s.currentRoom &&
+        s.rooms[s.currentRoom] &&
+        s.rooms[s.currentRoom].messages) ||
+      []
+  );
 
-    const currentRoom = useSelector(s => s.currentRoom);
-    const users = useSelector(s => s.currentRoom && s.rooms[s.currentRoom] && s.rooms[s.currentRoom].users || []);
-    const messages = useSelector(s => s.currentRoom && s.rooms[s.currentRoom] && s.rooms[s.currentRoom].messages || []);
+  const [messageInputText, setMessageInputText] = useState("");
 
-    const [messageInputText, setMessageInputText] = useState('');
+  useEffect(() => {
+    const query = queryString.parse(location.search);
 
-    useEffect(() => {
-
-        const query = queryString.parse(location.search);
-        // const room = currentRoom || query.room;
-        // const name = userName || query.name;
-        // console.log(name)
-        // console.log(room)
-
-        let room, name;
-        if (currentRoom) {
-            room=currentRoom
-        } else if (query.room) {
-            room = query.room;
-            setRoom(room);
-        }
-
-        if (userName) {
-            name = userName;
-        } else if (query.name) {
-            name = query.name;
-            setName(name);
-        }
-
-        if (!joinedRooms.length) {
-               socket = io(ENDPOINT);
-
-               socket.on("roomData", ({ room, users }) => {
-                roomUsersUpdated(room, users);
-
-                socket.on('message', ({ user, text, room }) => {
-                    addMessage(user, text, room);
-                });
-           });
-
-        }
-
-        if (!joinedRooms.includes(room)) {
-            socket.emit('join', { name, room }, error => {
-                if (error) {
-                    alert(error);
-                }
-            });
-
-            joinedRooms.push(room);
-         }
-
-    }, [location.search, currentRoom, userName]);
-
-    const sendMessage = e => {
-        e.preventDefault();
-
-        if (messageInputText) {
-            socket.emit('sendMessage', { room: currentRoom, text: messageInputText }, () => setMessageInputText(''));
-        }
+    let room, name;
+    if (currentRoom) {
+      room = currentRoom;
+    } else if (query.room) {
+      room = query.room;
+      setRoom(room);
     }
 
-    return (
-        <div className="outerContainer">
-            <div className="container">
-                <Paper className='topicsWindow'>
-                    <TopicsWindow rooms={rooms} currentUserName={userName}/>
-                </Paper>
-                <div className='chatbox-wrapper'>
-                    <InfoBar room={currentRoom} users={users} />
-                    <Messages messages={messages} currentUserName={userName} />
-                    <Input message={messageInputText} setText={setMessageInputText} sendMessage={sendMessage} />
-                </div>
-            </div>
-        </div>
-    );
-}
+    if (userName) {
+      name = userName;
+    } else if (query.name) {
+      name = query.name;
+      setName(name);
+    }
 
-const mapDispatchToProps = {
-    setRoom,
-    setName,
-    addMessage,
-    roomUsersUpdated,
+    if (!joinedRooms.length) {
+      socket = io(ENDPOINT);
+
+      socket.on("roomData", ({ room, users }) => {
+        roomUsersUpdated(room, users);
+
+        socket.on("message", ({ user, text, room }) => {
+          addMessage(user, text, room);
+        });
+      });
+    }
+
+    if (!joinedRooms.includes(room)) {
+      socket.emit("join", { name, room }, error => {
+        if (error) {
+          alert(error);
+        }
+      });
+
+      joinedRooms.push(room);
+    }
+  }, [location.search, currentRoom, userName]);
+
+  const sendMessage = e => {
+    e.preventDefault();
+
+    if (messageInputText) {
+      socket.emit(
+        "sendMessage",
+        { room: currentRoom, text: messageInputText },
+        () => setMessageInputText("")
+      );
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <Paper className="topicsWindow">
+          <TopicsWindow rooms={rooms} currentUserName={userName} />
+        </Paper>
+        <div className="chatbox-wrapper">
+          <InfoBar room={currentRoom} users={users} />
+          <Messages messages={messages} currentUserName={userName} />
+          <Input
+            message={messageInputText}
+            setText={setMessageInputText}
+            sendMessage={sendMessage}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(ChatComponent);
+const mapDispatchToProps = {
+  setRoom,
+  setName,
+  addMessage,
+  roomUsersUpdated
+};
+
+export default connect(null, mapDispatchToProps)(ChatComponent);
